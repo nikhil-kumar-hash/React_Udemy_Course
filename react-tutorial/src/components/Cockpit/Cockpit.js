979b6cc8eb5118ee479710c4d3b920ccac306d34
Nikhil <nikhil.kumar@kreditzy.com>
2020-12-30 12:44:34 +0530

    Styling changes
    Signed-off-by: Nikhil <nikhil.kumar@kreditzy.com>

---
+++ react-tutorial/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Cockpit.css'
+import styled from 'styled-components';
+
+
+const StyledButton = styled.button `
+
+background-color : ${props => props.alt ? 'red' : 'green'};
+color : white;
+font : inherit;
+border : 1px solid blue;
+padding : 8px;
+cursor : pointer;
+
+&:hover {
+  background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+  color : black;
+}
+
+`
+
+const cockpit = (props) => {
+
+    let classes = [];
+    if(props.persons.length <= 2) classes.push('red')
+
+    if(props.persons.length <= 1) classes.push('bold')
+
+    return (
+        <div className="Cockpit">
+        <h1>Hi I'm a React App</h1>
+        <p className={classes.join(' ')}> This is really working</p>
+        <StyledButton onClick={props.toggled}> Toggle Display </StyledButton>
+       </div>
+    );
+}
+
+export default cockpit;
--- react-tutorial/src/containers/App.js
+++ react-tutorial/src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
-
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+ 
 class App extends Component {
 
   state = {
@@ -60,46 +61,26 @@ class App extends Component {
 
 
     render() {
-
-      const style = {
-        backgroundColor : 'white',
-        font : 'inherit',
-        border : '1px solid blue',
-        padding : '8px',
-        cursor : 'pointer'
-      };
-
       let persons = null;
 
       if(this.state.showPersons) {
         persons = (
           <div>
-            {this.state.persons.map((person,index) => {
-              return <Person
-               name ={person.name}
-                age = {person.age}
-                click = {this.deletePersonHandler.bind(this,index)}
-                key={person.id}
-                changed={(event) => {this.nameChangeHandler(event, person.id)}}
-                />
-            })}
+            <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangeHandler}/>
           </div>
         )
       }
 
-      let classes = [];
-      if(this.state.persons.length <= 2) classes.push('red')
-
-      if(this.state.persons.length <= 1) classes.push('bold')
-
-
-
     return (
       <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <p className={classes.join(' ')}> This is really working</p>
-        <button style = {style} onClick={this.togglePersonsHandler}> Toggle Display </button>
-       {persons}
+        <Cockpit
+        persons={this.state.persons}
+        toggled={this.togglePersonsHandler}
+        />
+        {persons}
       </div>
     );
    // return React.createElement('div',{className : 'App'},React.createElement('h1',null,'Hi I am a React App!!')) 
